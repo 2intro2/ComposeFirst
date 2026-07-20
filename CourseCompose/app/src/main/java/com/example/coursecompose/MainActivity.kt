7065e68bc4c72e50ca2e33d2a6b9a1ed4cd623b0
intro222 <intro222@outlook.com>
2026-07-21 00:57:50 +0800

    feat: Modifier速览 -story ComposeFirst(1.7)

--- CourseCompose/app/src/main/java/com/example/coursecompose/MainActivity.kt
+++ CourseCompose/app/src/main/java/com/example/coursecompose/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.coursecompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,7 +25,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.lifecycleScope
 import com.example.coursecompose.ui.theme.CourseComposeTheme
 import kotlinx.coroutines.delay
@@ -30,24 +42,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CourseComposeTheme {
-                Column (Modifier.safeContentPadding(), content = {
-                    var name by remember {  mutableStateOf("李泓锟") }
-                    CharCounter(name)
-
-                    lifecycleScope.launch {
-                        delay(3000)
-                        name = "lh"
-                    }
-                })
-            }
+              Box(Modifier.safeDrawingPadding()) {
+                  LHK()
+              }
         }
-    }
 
-    @Composable
-    fun CharCounter(value: String) {
-        val length = remember(value) { value.length }
-        Text("字符串长度: ${length}")
     }
-
 }
+
+@Composable
+private fun LHK() {
+    Column(Modifier
+        .padding(8.dp)
+        .clickable {
+
+        }
+        .background(Color.Green, RoundedCornerShape(12.dp))
+        .padding(8.dp)
+        .background(Color.Red)
+    ) {
+        Image(
+            painterResource(R.drawable.avatar_diuwuxian),
+            "头像",
+            Modifier.size(120.dp).clip(CircleShape)
+        )
+
+    }
+}
+
+
